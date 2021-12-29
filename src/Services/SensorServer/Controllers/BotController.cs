fca7abd442e71a2191e8c84d065a8df4fb93cc67
Andrey <andrey@tstoryteller.com>
2021-12-29 11:58:27 +0200

    add SeriLog

--- src/Services/SensorServer/Controllers/BotController.cs
+++ src/Services/SensorServer/Controllers/BotController.cs
@@ -1,16 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SensorServer.Services;
 using Telegram.Bot.Types;
 
 namespace SensorServer.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+    [Route("[controller]")]
     public class BotController : ControllerBase
     {
         [HttpPost]
-        [Route("tg")]
         public async Task<IActionResult> Post([FromServices] HandleTelegramUpdateService handleTelegram,
                                           [FromBody] Update update)
         {
