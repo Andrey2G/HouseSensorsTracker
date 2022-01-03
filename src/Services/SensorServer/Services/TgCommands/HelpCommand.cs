1c14057336ef0853efc2ca6463adcaf5e782cd45
Andrey <andrey@tstoryteller.com>
2022-01-03 18:10:05 +0200

    update help command

--- src/Services/SensorServer/Services/TgCommands/HelpCommand.cs
+++ src/Services/SensorServer/Services/TgCommands/HelpCommand.cs
@@ -14,11 +14,13 @@ namespace SensorServer.Services.TgCommands
             return await botClient.SendTextMessageAsync(message.Chat.Id,
                  "Commands:\n" +
                                  "/ping - check the Bot liveness\n" +
-                                 "/heathpoint_water_current - Current temperature from sensors in heatpoint of hot water\n" +
-                                 "/heathpoint_water_avg_hour - Avarage temperature from sensors in heatpoint of hot water during the last hour\n" +
-                                 "/heathpoint_water_avg_day - Avarage temperature from sensors in heatpoint of hot water during the last day\n" +
-                                 "/heathpoint_water_avg_week - Avarage temperature from sensors in heatpoint of hot water during the last week\n" +
-                                 "/heathpoint_water_avg_month - Avarage temperature from sensors in heatpoint of hot water during the last month\n" +
+                                 "/heathpoint_temperature_current - Current temperature from sensors at the heatpoint of hot water\n" +
+                                 "/heathpoint_humidity_current - Current humidity value at the heatpoint of hot water\n" +
+                                 "/heathpoint_pressure_current - Current pressure value at the heatpoint of hot water\n" +
+                                 //"/heathpoint_water_avg_hour - Avarage temperature from sensors in heatpoint of hot water during the last hour\n" +
+                                 //"/heathpoint_water_avg_day - Avarage temperature from sensors in heatpoint of hot water during the last day\n" +
+                                 //"/heathpoint_water_avg_week - Avarage temperature from sensors in heatpoint of hot water during the last week\n" +
+                                 //"/heathpoint_water_avg_month - Avarage temperature from sensors in heatpoint of hot water during the last month\n" +
                                  "/help - get information about the Bot commands"
                 );
         }
